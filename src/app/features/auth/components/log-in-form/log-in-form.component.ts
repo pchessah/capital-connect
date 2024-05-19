10601c1b8f3aa9e50217891074351a5d8a16e756
Paul Chesa <paulchesa1@gmail.com>
2024-05-19 19:47:42 +0300

    Created auth module components

---
+++ src/app/features/auth/components/log-in-form/log-in-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-in-form.component.html',
+  styleUrl: './log-in-form.component.scss'
+})
+export class LogInFormComponent {
+
+}
---
+++ src/app/features/auth/components/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-form.component.html',
+  styleUrl: './sign-in-form.component.scss'
+})
+export class SignInFormComponent {
+
+}
