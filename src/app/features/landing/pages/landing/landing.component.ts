3b29cd9ad50f1540fb81c5a394512301c56c41e9
Paul Chesa <paulchesa1@gmail.com>
2024-05-21 23:01:30 +0300

    update scss

--- src/app/features/landing/pages/landing/landing.component.ts
+++ src/app/features/landing/pages/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SignInFormComponent } from '../../../auth/components/sign-in-form/sign-in-form.component';
+import { LogInFormComponent } from '../../../auth/components/log-in-form/log-in-form.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SignInFormComponent, LogInFormComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
