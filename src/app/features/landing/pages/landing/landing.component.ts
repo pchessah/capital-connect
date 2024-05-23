7d5dc2196383615a9ebf5e2a03c4674bbb8e91ec
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-23 07:38:41 +0300

    implemented section of the landing page

--- src/app/features/landing/pages/landing/landing.component.ts
+++ src/app/features/landing/pages/landing/landing.component.ts
@@ -1,12 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SignInFormComponent } from '../../../auth/components/sign-in-form/sign-in-form.component';
+import { CommonModule } from '@angular/common';
+import { CarouselComponent } from '../../../carousel/carousel.component';
 import { LogInFormComponent } from '../../../auth/components/log-in-form/log-in-form.component';
+import { SignInFormComponent } from '../../../auth/components/sign-in-form/sign-in-form.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, SignInFormComponent, LogInFormComponent],
+  imports: [CommonModule, SignInFormComponent, LogInFormComponent, CarouselComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
