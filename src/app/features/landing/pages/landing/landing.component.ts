2154c7f72221852a785f2f29e4a4dd9ce5e533b1
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-23 18:34:45 +0300

    defined rules for conditional form rendering

--- src/app/features/landing/pages/landing/landing.component.ts
+++ src/app/features/landing/pages/landing/landing.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../../../carousel/carousel.component';
 import { LogInFormComponent } from '../../../auth/components/log-in-form/log-in-form.component';
 import { SignUpFormComponent } from '../../../auth/components/sign-up-form/sign-up-form.component';
+
+enum FORMS { SIGNUP, SIGNIN }
 
 @Component({
   selector: 'app-landing',
@@ -11,6 +13,10 @@ import { SignUpFormComponent } from '../../../auth/components/sign-up-form/sign-
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
 
+export class LandingComponent {
+  forms =FORMS
+  active_form =this.forms.SIGNUP;
+  seSignup(){this.active_form =this.forms.SIGNUP};
+  seSignin(){this.active_form =this.forms.SIGNIN};
 }
