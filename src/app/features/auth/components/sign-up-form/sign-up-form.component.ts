45ab1c9a2483daa96bc874a570bb30f3cb8c3adc
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-23 18:36:17 +0300

    defined password security checks
    Importes CommonModule and the NgModule for iterations but iteration failed

--- src/app/features/auth/components/sign-up-form/sign-up-form.component.ts
+++ src/app/features/auth/components/sign-up-form/sign-up-form.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { AuthModule } from '../../modules/auth.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-sign-in-form',
+  selector: 'app-sign-up-form',
   standalone: true,
-  imports: [AuthModule],
+  imports: [CommonModule, AuthModule],
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.scss'
 })
 export class SignUpFormComponent {
-
+  password_validation_checks =[
+    {check: 'Password Strength', value: 'Weak'},
+    {check: 'Cannot contain your name or email address'},
+    {check: 'At least 8 charatcers'},
+    {check: 'Contains a number or a symbol'},
+  ]
 }
