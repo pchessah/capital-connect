7deaa91a6d5de564a6bc46f0fa428288f7367585
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-25 20:58:16 +0300

    added the organization's route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,14 @@ export const routes: Routes = [
     path: 'landing',
     loadChildren: () => import('./features/landing/modules/landing/landing.routing.module').then(m => m.LandingRoutingModule)
   },
+  
   {
     path: '', redirectTo: '/landing', pathMatch: 'full'
-  }
+  },
+  
+  {
+    path: 'organization',
+    loadChildren: () =>import('./features/organization/modules/organization/organization.routing').then(m =>m.OrganizationRoutingModule),
+
+  },
 ];
