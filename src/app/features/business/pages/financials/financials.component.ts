91bd682771d2395eefd3b3c330bef8bc84e2fd5d
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-03 11:37:28 +0300

    added asteppers

--- src/app/features/business/pages/financials/financials.component.ts
+++ src/app/features/business/pages/financials/financials.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../core/navbar/navbar.component';
 import { IndexComponent } from '../../components/index/index.component';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-financials',
   standalone: true,
-  imports: [NavbarComponent, IndexComponent],
+  imports: [NavbarComponent, IndexComponent, ProgressBarComponent],
   templateUrl: './financials.component.html',
   styleUrl: './financials.component.scss'
 })
