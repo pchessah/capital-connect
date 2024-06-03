d286c1e410eb3e7e57714cdf97a572662d0da08f
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-03 17:08:30 +0300

    added an event emitter

--- src/app/features/business/components/index/index.component.ts
+++ src/app/features/business/components/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
+  @Output() nextStep = new EventEmitter<number>();
+
+  setNextStep() {
+    this.nextStep.emit(1);
+  }
 
 }
