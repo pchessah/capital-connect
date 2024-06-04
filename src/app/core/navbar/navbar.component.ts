17cbacb39a774892f0d30785a5c40a4cabbd8e6c
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-04 16:19:18 +0300

    added a flag to check if the drawer is showing or not

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  drawer_showing =false;
+  toggleDrawer(){ this.drawer_showing =!this.drawer_showing; }
 }
