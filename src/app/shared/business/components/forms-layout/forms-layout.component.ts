ec251826589b2efe124b71e592354c0bedeb42e9
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-05 17:58:44 +0300

    added shared components

--- src/app/shared/business/components/forms-layout/forms-layout.component.ts
+++ src/app/shared/business/components/forms-layout/forms-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { ProgressBarComponent } from '../../../../features/business/components/progress-bar/progress-bar.component';
 import { BusinessPageService } from '../../../../core/business.page.service';
+import { ProgressBarComponent } from '../../../../features/business/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-forms-layout',
@@ -11,24 +11,25 @@ import { BusinessPageService } from '../../../../core/business.page.service';
 })
 export class FormsLayoutComponent {
   current_step =1;
-  current_page =1;
+  current_page =2;
   constructor(private businessPageService: BusinessPageService) {}
 
   @Input() steps: number =3;
+  @Input() title: string ='';
 
   setNextScreen(step: number) {
     this.setNextStep(step);
-    if (this.current_step >=this.steps || (this.current_step <1 && step <0)){
+    if (this.current_step >this.steps || (this.current_step <1 && step <0)){
+      this.current_step =1;
       this.current_page +=step;
+      this.businessPageService.setCurrentStep(this.current_step);
       this.businessPageService.setCurrentPage(this.current_page);
-      
     }
   }
 
   setNextStep(step =1){
-    if ((step <0 && this.current_step <1) || (step >0 && this.current_step >=this.steps)) return;
+    if ((step <0 && this.current_step <1) || (step >0 && this.current_step >this.steps)) return;
     this.current_step +=step;
-    this.businessPageService.setCurrentStep(this.current_step)
-
+    this.businessPageService.setCurrentStep(this.current_step);
   }
 }
