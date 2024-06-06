9fdc7a262147f6efd2230d01d13eca27f8daa6f3
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-06 13:10:52 +0300

    routed investor preparednes page

--- src/app/features/business/modules/business.routing.ts
+++ src/app/features/business/modules/business.routing.ts
@@ -5,9 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'financials', loadComponent: () => import('../pages/financials/financials.component').then(c => c.FinancialsComponent)},
   { path: 'investor-eligibility', loadComponent: () => import('../pages/investor-eligibility/investor-eligibility.component').then(c =>c.InvestorEligibilityComponent)},
+  { path: 'investor-preparedness', loadComponent: () => import('../pages/investor-preparedness/investor-preparedness.component').then(c =>c.InvestorPreparednessComponent)},
 ]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
-}) 
+})
 export class BusinessROutingModule { }
