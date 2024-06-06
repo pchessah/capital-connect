770dc7e20c25709807a6c2b17dd2324f96524acb
Meltus Bwire <165395433+j-netcom-dev@users.noreply.github.com>
2024-06-06 14:10:27 +0300

    Issue #22 - Designed investor preparedness flow (#23)
    * #FIX
    
    removed subscrbe and used async pipes instead
    
    * routed investor preparednes page
    
    * implemented the investor preparedness page

--- src/app/features/business/pages/investor-preparedness/investor-preparedness.component.ts
+++ src/app/features/business/pages/investor-preparedness/investor-preparedness.component.ts
@@ -2,22 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusinessPageService } from '../../../../core/business.page.service';
 import { LayoutComponent } from '../../../../shared/business/layout/layout.component';
-import { IndexComponent } from '../../components/investor-eligibility/index/index.component';
-import { StepsComponent } from '../../components/investor-eligibility/steps/steps.component';
 import { IndexLayoutComponent } from '../../../../shared/business/components/index-layout/index-layout.component';
 import { FormsLayoutComponent } from '../../../../shared/business/components/forms-layout/forms-layout.component';
-import { SuccessScreenComponent } from '../../components/investor-eligibility/success-screen/success-screen.component';
-import { tap } from 'rxjs';
+import { IndexComponent } from '../../components/investor-preparedness/index/index.component';
+import { StepsComponent } from '../../components/investor-preparedness/steps/steps.component';
+import { SuccessScreenComponent } from '../../components/investor-preparedness/success-screen/success-screen.component';
 
 @Component({
-  selector: 'app-investor-eligibility',
+  selector: 'app-investor-preparedness',
   standalone: true,
   imports: [CommonModule, LayoutComponent, IndexLayoutComponent, FormsLayoutComponent, IndexComponent, StepsComponent,SuccessScreenComponent],
-  templateUrl: './investor-eligibility.component.html',
-  styleUrl: './investor-eligibility.component.scss'
+  templateUrl: './investor-preparedness.component.html',
+  styleUrl: './investor-preparedness.component.scss'
 })
-export class InvestorEligibilityComponent {
+export class InvestorPreparednessComponent {
+  
   constructor(private screenService: BusinessPageService) {}
-
   currentPage$ = this.screenService.current_page$
 }
