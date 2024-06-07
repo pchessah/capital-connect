f3011f162d575f2d6c354c5d8ddaff02f99a34b2
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-07 08:28:54 +0300

    #feat: converted login form to reactive form

--- src/app/features/auth/components/log-in-form/log-in-form.component.ts
+++ src/app/features/auth/components/log-in-form/log-in-form.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { AuthModule } from '../../modules/auth.module';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-log-in-form',
   standalone: true,
-  imports: [AuthModule],
+  imports: [CommonModule, AuthModule, FormsModule, ReactiveFormsModule],
   templateUrl: './log-in-form.component.html',
   styleUrl: './log-in-form.component.scss'
 })
 export class LogInFormComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  signInForm = this._formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+  listenToFormChanges$ = this.signInForm.valueChanges.pipe(
+    tap(credentials => {})
+  );
+
+  isTouchedOrDirty(formControlName:string){
+
+    const fieldIsTouched =this.signInForm.get(formControlName)?.touched;
+    const fieldIsDirty =this.signInForm.get(formControlName)?.dirty;
+    return fieldIsTouched || fieldIsDirty;
+  }
+
+  isValid(formControlName:string){
+    return  this.signInForm.get(formControlName)?.valid;
+  }
+
+  submitCredentials(){
+    const credentials = this.signInForm.value;
+    debugger
+  }
 }
