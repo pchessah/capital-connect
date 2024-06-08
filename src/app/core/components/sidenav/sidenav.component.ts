be100d1c08903408726a77b85149c93f54db618d
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-08 17:55:48 +0300

    created side nav

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {ProBadgeComponent} from "../pro-badge/pro-badge.component";
 import {SharedModule} from "../../../shared/shared.module";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [ProBadgeComponent, SharedModule],
+  imports: [ProBadgeComponent, SharedModule, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent {
 
+export class SidenavComponent {
+  show_nav =true;
+  toggle_navbar(){
+    this.show_nav = !this.show_nav;
+  }
 }
