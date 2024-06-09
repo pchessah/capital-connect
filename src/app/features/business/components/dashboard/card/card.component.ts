9bf3e30d87848517e0ae90aa922dcfe9341f3567
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 22:03:09 +0300

    added dynamic content to dashboard cards

--- src/app/features/business/components/dashboard/card/card.component.ts
+++ src/app/features/business/components/dashboard/card/card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { SharedModule } from "../../../../../shared/shared.module";
+
+type DETAIL_TYPE ={
+  title: string,
+  value: string,
+  period?: string,
+  featured: boolean,
+}
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+    imports: [CommonModule, SharedModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() detail: DETAIL_TYPE | undefined;
 }
