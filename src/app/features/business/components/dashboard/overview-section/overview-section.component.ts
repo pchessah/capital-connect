1ea97918369eb129e2fb507ebf747712b18e2f0d
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 22:34:45 +0300

    feat: created and installed an image section

--- src/app/features/business/components/dashboard/overview-section/overview-section.component.ts
+++ src/app/features/business/components/dashboard/overview-section/overview-section.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../../../../shared/shared.module";
 import {CardComponent} from "../card/card.component";
+import {PhotoCollageComponent} from "../photo-collage/photo-collage.component";
 
 @Component({
   selector: 'app-overview-section',
   standalone: true,
-  imports: [CommonModule, SharedModule, CardComponent],
+  imports: [CommonModule, SharedModule, CardComponent, PhotoCollageComponent],
   templateUrl: './overview-section.component.html',
   styleUrl: './overview-section.component.scss'
 })
