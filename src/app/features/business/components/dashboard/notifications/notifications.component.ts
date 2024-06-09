c08c674a39b9874ec52bf136ef67a27580f7736a
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 21:22:38 +0300

    #feat: created tile

--- src/app/features/business/components/dashboard/notifications/notifications.component.ts
+++ src/app/features/business/components/dashboard/notifications/notifications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TileComponent} from "../tile/tile.component";
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [TileComponent],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
