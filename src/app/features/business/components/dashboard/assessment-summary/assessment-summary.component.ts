ca81c607b756e013881a80f5c01340db93de0c00
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 20:20:35 +0300

    styled link

--- src/app/features/business/components/dashboard/assessment-summary/assessment-summary.component.ts
+++ src/app/features/business/components/dashboard/assessment-summary/assessment-summary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {SharedModule} from "../../../../../shared/shared.module";
 
 @Component({
   selector: 'app-assessment-summary',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './assessment-summary.component.html',
   styleUrl: './assessment-summary.component.scss'
 })
