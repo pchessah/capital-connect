f4b1177a7632bbb1731e49c8d4b217b0d6cb80cf
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 15:37:17 +0300

    feat:
    Installed the schedules components to the main component

--- src/app/features/business/components/dashboard/main/main.component.ts
+++ src/app/features/business/components/dashboard/main/main.component.ts
@@ -5,13 +5,15 @@ import {NotificationsComponent} from "../notifications/notifications.component";
 import {AssessmentSummaryComponent} from "../assessment-summary/assessment-summary.component";
 import {AdvertisementSpaceComponent} from "../advertisement-space/advertisement-space.component";
 import {OverviewSectionComponent} from "../overview-section/overview-section.component";
+import {SchedulesSectionComponent} from "../schedules-section/schedules-section.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
     NavbarComponent, ProfileStatusComponent, NotificationsComponent,
-    AssessmentSummaryComponent, AdvertisementSpaceComponent, OverviewSectionComponent
+    AssessmentSummaryComponent, AdvertisementSpaceComponent, OverviewSectionComponent,
+    SchedulesSectionComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
