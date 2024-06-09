af299ceb445cac786f25d948ad128764067a3fcd
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-09 19:57:44 +0300

    fix
    fixed the layout inconsistence of the advert card

--- src/app/features/business/components/dashboard/advertisement-space/advertisement-space.component.ts
+++ src/app/features/business/components/dashboard/advertisement-space/advertisement-space.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {SharedModule} from "../../../../../shared/shared.module";
 
 @Component({
   selector: 'app-advertisement-space',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './advertisement-space.component.html',
   styleUrl: './advertisement-space.component.scss'
 })
