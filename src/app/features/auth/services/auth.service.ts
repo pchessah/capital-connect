2625530caf987af86032defdbd2e5fd4313dfb38
Paul Chesa <paulchesa1@gmail.com>
2024-06-10 09:42:52 +0300

    feat: Created new service to handle authentication

---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { BaseHttpService } from '../../../core/http/base/base.http.service';
+import { CreateUserInput } from '../interfaces/auth.interface';
+import { HttpClient } from '@angular/common/http';
+import { BASE_URL } from '../../../core/http/base/constants';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService extends BaseHttpService<CreateUserInput> {
+
+
+  constructor(private _httpClient: HttpClient) {
+    super(_httpClient)
+  }
+
+  signUpUser(user: CreateUserInput) {
+    return this.create(`${BASE_URL}/auth/signup`, user)
+  }
+
+}
