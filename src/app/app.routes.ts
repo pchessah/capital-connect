75506ccefb309b766c21910cb9da3df30f12dc00
Paul Chesa <paulchesa1@gmail.com>
2024-06-12 00:34:22 +0300

    feat: Implement use of guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { isLoggedInCanActivateChildGuard, isLoggedInCanActivateGuard } from './shared/guards/isLoggedIn.guard';
 
 export const routes: Routes = [
   {
@@ -11,13 +12,19 @@ export const routes: Routes = [
   {
     path: 'organization',
     loadChildren: () => import('./features/organization/modules/organization/organization.routing').then(m => m.OrganizationRoutingModule),
+    canActivate:[isLoggedInCanActivateGuard],
+    canActivateChild: [isLoggedInCanActivateChildGuard]
   },
   {
     path: 'business',
     loadChildren: () => import('./features/business/modules/business.routing').then(m => m.BusinessRoutingModule),
+    canActivate:[isLoggedInCanActivateGuard],
+    canActivateChild: [isLoggedInCanActivateChildGuard]
   },
   {
     path: 'questions',
-    loadChildren: () => import('./features/questions/questions.routing').then(m => m.QuestionsRoutingModule)
+    loadChildren: () => import('./features/questions/questions.routing').then(m => m.QuestionsRoutingModule),
+    canActivate:[isLoggedInCanActivateGuard],
+    canActivateChild: [isLoggedInCanActivateChildGuard]
   }
 ];
