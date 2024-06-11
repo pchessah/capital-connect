012521819a631dc3f1a6ed448a65a597ae20f738
Paul Chesa <paulchesa1@gmail.com>
2024-06-12 00:34:07 +0300

    feat: Added auth interceptor in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,19 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { HttpErrorInterceptor } from './shared/interceptors/http-error.interceptor';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
+import { AuthInterceptor } from './shared/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(MatDialogModule, MatIconModule),
-    provideHttpClient(withInterceptors([HttpErrorInterceptor]))
+    provideHttpClient(withInterceptors([HttpErrorInterceptor, AuthInterceptor]))
   ]
 };
