391de7dabf0dccbe3c5700bc48f99ca8c4902c80
Paul Chesa <paulchesa1@gmail.com>
2024-06-11 08:21:09 +0300

    chore: Minor code formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,16 @@ export const routes: Routes = [
     path: 'landing',
     loadChildren: () => import('./features/landing/modules/landing/landing.routing.module').then(m => m.LandingRoutingModule)
   },
-  
+  {
+    path: 'organization',
+    loadChildren: () =>import('./features/organization/modules/organization/organization.routing').then(m =>m.OrganizationRoutingModule),
+  },
+  {
+    path: 'business',
+    loadChildren: () =>import('./features/business/modules/business.routing').then(m =>m.BusinessRoutingModule),
+  },  
   {
     path: '', redirectTo: '/landing', pathMatch: 'full'
   },
   
-  {
-    path: 'organization',
-    loadChildren: () =>import('./features/organization/modules/organization/organization.routing').then(m =>m.OrganizationRoutingModule),
-
-  },
-
-  {
-    path: 'business',
-    loadChildren: () =>import('./features/business/modules/business.routing').then(m =>m.BusinessROutingModule),
-
-  },
 ];
