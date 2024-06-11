f9e7a418cf8def50dba5df227711f7752ef524ce
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-11 12:43:33 +0300

    added a steps enum

--- src/app/features/business/components/dashboard/profile-status/profile-status.component.ts
+++ src/app/features/business/components/dashboard/profile-status/profile-status.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../../../../shared/shared.module";
-import {STEP_STATUS} from "../../../interfaces/profile.status.enum";
+import {STEP_STATUS, STEP_TYPE} from "../../../interfaces/status.enum";
 
 
 
@@ -15,9 +15,9 @@ import {STEP_STATUS} from "../../../interfaces/profile.status.enum";
 export class ProfileStatusComponent {
   currentStep:number = 2;
   STATUS = STEP_STATUS;
-  steps =[
-    {title: 'Business Financials', status: 'complete'},
-    {title: 'Investor Eligibility', status: 'incomplete'},
-    {title: 'Investor Preparedness', status: 'incomplete'},
+  steps: STEP_TYPE[] =[
+    {title: 'Business Financials', status: STEP_STATUS.COMPLETE},
+    {title: 'Investor Eligibility', status: STEP_STATUS.INCOMPLETE},
+    {title: 'Investor Preparedness', status: STEP_STATUS.INCOMPLETE},
   ]
 }
--- src/app/features/business/interfaces/status.enum.ts
+++ src/app/features/business/interfaces/status.enum.ts
@@ -2,3 +2,8 @@ export enum STEP_STATUS {
   COMPLETE ='complete',
   INCOMPLETE = 'incomplete',
 }
+
+export type STEP_TYPE ={
+  title: string,
+  status: STEP_STATUS,
+}
