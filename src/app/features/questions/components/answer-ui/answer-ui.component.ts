9106949a121e16939dda3ae643d1d70567b96a89
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-18 17:14:27 +0300

    created an answers ui

---
+++ src/app/features/questions/components/answer-ui/answer-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './answer-ui.component.html',
+  styleUrl: './answer-ui.component.scss'
+})
+export class AnswerUiComponent {
+
+}
--- src/app/features/questions/components/ui/ui.component.ts
+++ src/app/features/questions/components/ui/ui.component.ts
@@ -3,6 +3,7 @@ import {NgOptimizedImage} from "@angular/common";
 import {QuestionUiComponent} from "../question-ui/question-ui.component";
 import {QuestionSubsectionUiComponent} from "../question-subsection-ui/question-subsection-ui.component";
 import {QuestionTypeUiComponent} from "../question-type-ui/question-type-ui.component";
+import {AnswerUiComponent} from "../answer-ui/answer-ui.component";
 
 @Component({
   selector: 'app-ui',
@@ -12,6 +13,7 @@ import {QuestionTypeUiComponent} from "../question-type-ui/question-type-ui.comp
     QuestionUiComponent,
     QuestionSubsectionUiComponent,
     QuestionTypeUiComponent,
+    AnswerUiComponent,
   ],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.scss'
