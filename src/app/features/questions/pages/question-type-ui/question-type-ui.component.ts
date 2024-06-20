d179e5323c8082d547e3b78300c2c9cd35ad7acb
Meltus Bwire <165395433+j-netcom-dev@users.noreply.github.com>
2024-06-20 10:35:39 +0300

    Form service (#45)
    * defined question form routes
    
    * creted form builder

--- src/app/features/questions/pages/question-type-ui/question-type-ui.component.ts
+++ src/app/features/questions/pages/question-type-ui/question-type-ui.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {QUESTION_FORM_STEPS} from "../../../../shared/interfaces/question.form.steps.enum";
+import {UiComponent} from "../../components/ui/ui.component";
 
 @Component({
   selector: 'app-question-type-ui',
   standalone: true,
-  imports: [],
+  imports: [
+    UiComponent
+  ],
   templateUrl: './question-type-ui.component.html',
   styleUrl: './question-type-ui.component.scss'
 })
 export class QuestionTypeUiComponent {
-
+    protected readonly STEPS = QUESTION_FORM_STEPS;
 }
