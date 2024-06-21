da4ba0da368f4444235b7e3ff34822d3bdd6fc09
Paul Chesa <paulchesa1@gmail.com>
2024-06-21 20:14:57 +0300

    feat: Refine flow for submiting company info to the Backend

--- src/app/features/organization/pages/setup/setup.component.ts
+++ src/app/features/organization/pages/setup/setup.component.ts
@@ -5,6 +5,7 @@ import { StepTwoComponent } from '../../components/step-two/step-two.component';
 import { StepThreeComponent } from '../../components/step-three/step-three.component';
 import { StepFourComponent } from '../../components/step-four/step-four.component';
 import { OrganizationOnboardService } from '../../services/organization-onboard.service';
+import { catchError, EMPTY, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-setup',
@@ -15,19 +16,29 @@ import { OrganizationOnboardService } from '../../services/organization-onboard.
 })
 export class SetupComponent {
    _organizationOnboardService = inject(OrganizationOnboardService);
+   submitCompanyInfo$ = new Observable();
 
-  current_step = 0;
+  current_step = 1;
   steps = [1, 2, 3, 4];
 
   setStep(direction: number) {
-    if (direction > 0 && (this.current_step >= this.steps.length - 1)) return;
-    if (direction < 0 && (this.current_step <= 0)) return;
-    this.current_step += direction;
+    if (direction > 0 && (this.current_step >= this.steps.length)) return;
+    if (direction < 0 && (this.current_step <= 1)) return;
+
+    if(this.current_step === 3) {
+      this.submitCompanyInfo$ = this._organizationOnboardService.submitCompanyInfo().pipe(tap(res =>  this.current_step += direction), catchError(err => {
+        console.error('⚠️There was an error:', err)
+        return EMPTY
+      }))
+    } else {
+      this.current_step += direction;
+    }
   }
 
   get isDisabled(){
-    if(this.current_step === 0) return !this._organizationOnboardService.step1isValid();
-    if(this.current_step === 1) return !this._organizationOnboardService.step2isValid();
+    if(this.current_step === 1) return !this._organizationOnboardService.step1isValid();
+    if(this.current_step === 2) return !this._organizationOnboardService.step2isValid();
+    if(this.current_step === 3) return false //TODO: @pchessahadd condition to check if upload of logo is successful
     return true;
   }
 }
