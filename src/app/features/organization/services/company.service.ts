6cbf2993267783bb7196026628094fc3f6ca7a12
Paul Chesa <paulchesa1@gmail.com>
2024-06-21 20:14:05 +0300

    feat: Added http service for companies

---
+++ src/app/features/organization/services/company.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BASE_URL, BaseHttpService } from '../../../core';
+import { HttpClient } from '@angular/common/http';
+import { CompanyInput, CompanyResponse } from '../interfaces';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class CompanyHttpService extends BaseHttpService {
+  constructor(private _httpClient: HttpClient) { 
+    super(_httpClient);
+  }
+
+  createCompany(companyInput:CompanyInput) {
+    return this.create(`${BASE_URL}/company`, companyInput) as Observable<CompanyResponse>
+  }
+  
+}
