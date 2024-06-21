57cf369690be30b8a901d7b4fbaf416fdda34916
Paul Chesa <paulchesa1@gmail.com>
2024-06-21 20:47:44 +0300

    feat: Use local variable to track loading status created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared';
 import { LoadingComponent } from './core/components/loading/loading.component';
 import { LoadingService } from './core';
+import { tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, LoadingComponent],
+  imports: [CommonModule, RouterOutlet, SharedModule, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  loadingService = inject(LoadingService)
+  private _loadingService = inject(LoadingService);
+  isLoading = true;
+
+  trackLoadingStatus$ = this._loadingService.loading$.pipe(tap(res => this.isLoading = res))
+
+
 }
