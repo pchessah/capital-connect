9ebed86798901559724e07eab008c92f9314db66
Paul Chesa <paulchesa1@gmail.com>
2024-06-21 20:46:17 +0300

    feat: Added user interface

--- src/app/features/auth/interfaces/auth.interface.ts
+++ src/app/features/auth/interfaces/auth.interface.ts
@@ -16,3 +16,17 @@ export enum FORM_TYPE {
   SIGNUP, 
   SIGNIN 
 }
+
+export interface User {
+  id: number;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  roles: string;
+  resetPasswordToken: string | null;
+  resetPasswordExpires: string | null;
+  isEmailVerified: boolean;
+  emailVerificationToken: string;
+  emailVerificationExpires: string;
+}
