d9d0d14d4ebfc80cc0657930540dc0723f907f39
Paul Chesa <paulchesa1@gmail.com>
2024-06-21 20:45:28 +0300

    feat: Disable upload feature

--- src/app/features/organization/components/step-three/step-three.component.ts
+++ src/app/features/organization/components/step-three/step-three.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../../../../shared';
+import { FeedbackService } from '../../../../core';
 
 @Component({
   selector: 'app-step-three',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './step-three.component.html',
   styleUrl: './step-three.component.scss'
 })
 export class StepThreeComponent {
+
+  private _feedbackService = inject(FeedbackService);
+
+  displayMsg(){
+    this._feedbackService.info("Feature not available yet. Please Contact Admin")
+  }
+
+
 
 }
