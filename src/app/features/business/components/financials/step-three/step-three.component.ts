131dff88ece37803f0e7f34904fcf49837944890
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-24 09:27:20 +0300

    pulled dynamic business questions

--- src/app/features/business/components/financials/step-three/step-three.component.ts
+++ src/app/features/business/components/financials/step-three/step-three.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {QuestionsService} from "../../../../questions/services/questions/questions.service";
+import {tap} from "rxjs";
+import {Question} from "../../../../questions/interfaces";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-step-three',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgIf
+  ],
   templateUrl: './step-three.component.html',
   styleUrl: './step-three.component.scss'
 })
 export class StepThreeComponent {
+  private _questionService = inject(QuestionsService);
+  // subsections$ = this._questionService.getSubSectionsOfaSection(5).pipe(tap(res => {
+  //   debugger
+  // }))
 
+  questions$ =  this._questionService.getQuestionsOfSubSection(14).pipe(tap(questions => {
+    this.questions = questions
+  }))
+  questions: Question[] = [];
 }
