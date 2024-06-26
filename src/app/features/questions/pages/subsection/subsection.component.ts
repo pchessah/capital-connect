b04678d1566abc19beb0c997ce3bad7ef8738ec9
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-26 10:08:08 +0300

    styled the subsection

--- src/app/features/questions/pages/subsection/subsection.component.ts
+++ src/app/features/questions/pages/subsection/subsection.component.ts
@@ -4,11 +4,12 @@ import { FormStateService } from '../../services/form-state/form-state.service';
 import { CommonModule } from '@angular/common';
 import { Observable, tap } from 'rxjs';
 import { SharedModule } from '../../../../shared';
-import { Section, SubSection, SubSectionInput } from '../../interfaces';
-import { Router } from '@angular/router';
+import {Question, Section, SubSection, SubSectionInput} from '../../interfaces';
+import {ActivatedRoute, Router} from '@angular/router';
 import { QUESTION_FORM_STEPS } from "../../../../shared/interfaces/question.form.steps.enum";
 import { UiComponent } from "../../components/ui/ui.component";
 import { QuestionsService } from '../../services/questions/questions.service';
+import {QuestionCardComponent} from "../../components/question-card/question-card.component";
 
 @Component({
   selector: 'app-subsection',
@@ -17,26 +18,28 @@ import { QuestionsService } from '../../services/questions/questions.service';
     ReactiveFormsModule,
     CommonModule,
     SharedModule,
-    UiComponent
+    UiComponent,
+    QuestionCardComponent
   ],
   templateUrl: './subsection.component.html',
   styleUrl: './subsection.component.scss'
 })
-export class SubSectionComponent implements OnInit {
+export class SubSectionComponent{
   section!:Section;
-  ngOnInit(): void {
-    this._checkEditMode()
-  }
+  questions:Question[] =[];
+  // ngOnInit(): void {
+  //   this._checkEditMode()
+  // }
 
   protected readonly STEPS = QUESTION_FORM_STEPS;
-
+  private _activatedRoute = inject(ActivatedRoute);
   private _fb = inject(FormBuilder);
   private _formStateService = inject(FormStateService);
   private _router = inject(Router);
   private _questionsService = inject(QuestionsService);
 
   private _navState: { sectionId: number, subsectionId: number } = this._router.getCurrentNavigation()?.extras.state as any;
-  
+
   private _subsectionId: number = this._navState?.subsectionId;
   sectionId: number = this._navState?.sectionId
 
@@ -45,49 +48,57 @@ export class SubSectionComponent implements OnInit {
     description: ['', Validators.required]
   });
 
-  subsectionForm$ = this.subsectionForm.valueChanges.pipe(tap(vals => {
-    const input: SubSectionInput = {
-      sectionId: this.sectionId,
-      name: vals.name as string,
-      description: vals.description as string
-    }
-    this._formStateService.setSubsectionForm(input);
-    this._formStateService.setSubSectionFormIsValid(this.subsectionForm.valid);
-  }));
+  questions$ = this._activatedRoute.paramMap .pipe(tap((res) =>{
+    // @ts-ignore
+    const id =Number(res.params.id);
+    this._questionsService.getQuestionsOfSubSection(id).pipe(tap(vals => {
+      this.questions =vals;
+    })).subscribe()
+  }))
 
-  isSubsectionFormValid$ = this._formStateService.subsectionFormIsValid$.pipe(tap(isValid => {
-    this.isSubsectionFormValid = isValid;
-  }));
-
-  fetchedSubSection$ = new Observable();
-  fetchedSection$ = this._questionsService.getSingleSection(this.sectionId)
-
-  nextOperation$: Observable<SubSection> = new Observable()
-
-  isSubsectionFormValid = false;
-  editMode = false;
-  
-  private _checkEditMode() {
-    if (this._subsectionId) {
-      this.editMode = true;
-      this.fetchedSubSection$ = this._formStateService.getCurrentSubSectionBeingEdited(this._subsectionId).pipe(tap(subsection => {
-        this.subsectionForm.patchValue({
-          name: subsection.name,
-          description: subsection.description
-        });
-      }))
-    }
-  }
-
-  submit() {
-    const call$ = this.editMode ? this._formStateService.updateSubsection(this.sectionId, this._subsectionId)
-                                : this._formStateService.createSubsection(this.sectionId) 
-    this.nextOperation$ = call$.pipe(tap(res => {
-      this._router.navigate(['/questions'])
-    }));
-  }
-
-  cancel() {
-    this._router.navigate(['/questions'])
-  }
+  // subsectionForm$ = this.subsectionForm.valueChanges.pipe(tap(vals => {
+  //   const input: SubSectionInput = {
+  //     sectionId: this.sectionId,
+  //     name: vals.name as string,
+  //     description: vals.description as string
+  //   }
+  //   this._formStateService.setSubsectionForm(input);
+  //   this._formStateService.setSubSectionFormIsValid(this.subsectionForm.valid);
+  // }));
+  //
+  // isSubsectionFormValid$ = this._formStateService.subsectionFormIsValid$.pipe(tap(isValid => {
+  //   this.isSubsectionFormValid = isValid;
+  // }));
+  //
+  // fetchedSubSection$ = new Observable();
+  // fetchedSection$ = this._questionsService.getSingleSection(this.sectionId)
+  //
+  // nextOperation$: Observable<SubSection> = new Observable()
+  //
+  // isSubsectionFormValid = false;
+  // editMode = false;
+  //
+  // private _checkEditMode() {
+  //   if (this._subsectionId) {
+  //     this.editMode = true;
+  //     this.fetchedSubSection$ = this._formStateService.getCurrentSubSectionBeingEdited(this._subsectionId).pipe(tap(subsection => {
+  //       this.subsectionForm.patchValue({
+  //         name: subsection.name,
+  //         description: subsection.description
+  //       });
+  //     }))
+  //   }
+  // }
+  //
+  // submit() {
+  //   const call$ = this.editMode ? this._formStateService.updateSubsection(this.sectionId, this._subsectionId)
+  //                               : this._formStateService.createSubsection(this.sectionId)
+  //   this.nextOperation$ = call$.pipe(tap(res => {
+  //     this._router.navigate(['/questions'])
+  //   }));
+  // }
+  //
+  // cancel() {
+  //   this._router.navigate(['/questions'])
+  // }
 }
