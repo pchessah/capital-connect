27797e011502e558592418d2da2ea2d9223398d5
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-26 10:14:24 +0300

    styled section buttons

--- src/app/features/questions/components/section-card/section-card.component.ts
+++ src/app/features/questions/components/section-card/section-card.component.ts
@@ -3,11 +3,12 @@ import {SharedModule} from "../../../../shared";
 import { CommonModule} from "@angular/common";
 import {Observable, tap} from "rxjs";
 import {QuestionsService} from "../../services/questions/questions.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-section-card',
   standalone: true,
-  imports: [SharedModule, CommonModule],
+  imports: [SharedModule, CommonModule, RouterLink],
   templateUrl: './section-card.component.html',
   styleUrl: './section-card.component.scss'
 })
@@ -15,22 +16,12 @@ export class SectionCardComponent {
   @Input() name!: string;
   @Input() id!: number;
   sections_count =0;
-  questions_count = 0;
   subsections$ =new Observable();
   private _questionService = inject(QuestionsService);
   ngOnInit(){
     this.subsections$ = this._questionService.getSectionQuestions(this.id).pipe(
       tap(subsections => {
         this.sections_count = subsections.length;
-
-        subsections.forEach(subsection =>{
-          // @ts-ignore
-          if (subsection.questions) {
-            // @ts-ignore
-            this.questions_count += subsection?.questions.length
-          } else {
-            this.questions_count += 0
-          }})
       })
     );
   }
