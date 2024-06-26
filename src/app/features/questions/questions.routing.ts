725946b904458c5871a6280d87ad4c7e906b8c39
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-26 12:36:11 +0300

    routed edit endpoints

--- src/app/features/questions/questions.routing.ts
+++ src/app/features/questions/questions.routing.ts
@@ -8,15 +8,19 @@ const routes: Routes = [
 
   { path: 'section', loadComponent: () => import('./pages/section/section.component').then(c => c.SectionComponent) },
   { path: 'section/:id', loadComponent: () => import('./pages/section/section.component').then(c => c.SectionComponent) },
+  { path: 'section/:id/edit', loadComponent: () => import('./pages/edit-section/edit-section.component').then(c => c.EditSectionComponent) },
 
   { path: 'sub-section', loadComponent: () => import('./pages/subsection/subsection.component').then(c => c.SubSectionComponent) },
   { path: 'sub-section/:id', loadComponent: () => import('./pages/subsection/subsection.component').then(c => c.SubSectionComponent) },
+  { path: 'sub-section/:id/edit', loadComponent: () => import('./pages/edit-subsection/edit-subsection.component').then(c => c.EditSubsectionComponent) },
 
   { path: 'single-question', loadComponent: () => import('./pages/question-type-ui/question-type-ui.component').then(c => c.QuestionTypeUiComponent) },
   { path: 'single-question/:id', loadComponent: () => import('./pages/question-type-ui/question-type-ui.component').then(c => c.QuestionTypeUiComponent) },
+  { path: 'single-question/:id/edit', loadComponent: () => import('./pages/edit-question/edit-question.component').then(c => c.EditQuestionComponent) },
 
   { path: 'answers', loadComponent: () => import('./pages/answer-ui/answer-ui.component').then(c => c.AnswerUiComponent) },
   { path: 'answers/:id', loadComponent: () => import('./pages/answer-ui/answer-ui.component').then(c => c.AnswerUiComponent) },
+  { path: 'answers/:id/edit', loadComponent: () => import('./pages/edit-answer/edit-answer.component').then(c => c.EditAnswerComponent) },
 ]
 
 @NgModule({
