0e8113cec69ec6889036a54e2f337bc82a194d97
Paul Chesa <paulchesa1@gmail.com>
2024-06-27 14:21:13 +0300

    fix: Fix subsections page fetches

--- src/app/features/questions/pages/subsection/subsection.component.ts
+++ src/app/features/questions/pages/subsection/subsection.component.ts
@@ -2,14 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormStateService } from '../../services/form-state/form-state.service';
 import { CommonModule } from '@angular/common';
-import { Observable, tap } from 'rxjs';
+import { combineLatest, Observable, switchMap, tap } from 'rxjs';
 import { SharedModule } from '../../../../shared';
-import {Question, Section} from '../../interfaces';
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import { Question, Section } from '../../interfaces';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { QUESTION_FORM_STEPS } from "../../../../shared/interfaces/question.form.steps.enum";
 import { UiComponent } from "../../components/ui/ui.component";
 import { QuestionsService } from '../../services/questions/questions.service';
-import {QuestionCardComponent} from "../../components/question-card/question-card.component";
+import { QuestionCardComponent } from "../../components/question-card/question-card.component";
 
 @Component({
   selector: 'app-subsection',
@@ -25,12 +25,12 @@ import {QuestionCardComponent} from "../../components/question-card/question-car
   templateUrl: './subsection.component.html',
   styleUrl: './subsection.component.scss'
 })
-export class SubSectionComponent{
-  section!:Section;
-  questions:Question[] =[];
-  subSectionId!:number;
-  subsectionName!:string;
-  routeId!:string;
+export class SubSectionComponent {
+  section!: Section;
+  questions: Question[] = [];
+  subSectionId!: number;
+  subsectionName!: string;
+  routeId!: string;
 
   protected readonly STEPS = QUESTION_FORM_STEPS;
   private _activatedRoute = inject(ActivatedRoute);
@@ -39,21 +39,23 @@ export class SubSectionComponent{
   sectionId!: number;
 
 
-  sectionInfo$ =new Observable<Section>();
-  questionsFetch$ =new Observable<Question[]>();
-  questions$ = this._activatedRoute.params .pipe(tap((res) =>{
+  sectionInfo$ = new Observable<Section>();
+  questionsFetch$ = new Observable<Question[]>();
+  init$ = this._activatedRoute.params.pipe(switchMap((res) => {
     // @ts-ignore
-    this.routeId =res['id'].trim();
-    const ids =this.routeId.split('-');
-    this.sectionId =Number(ids.at(0))
-    this.subSectionId =Number(ids.at(1));
-    this.sectionInfo$ =this._questionsService.getSingleSubsection(this.subSectionId).pipe(tap(subsection =>{
-      this.subsectionName =subsection.name;
-    }))
+    this.routeId = res['id'].trim();
+    const ids = this.routeId.split('-');
+    this.sectionId = Number(ids.at(0))
+    this.subSectionId = Number(ids.at(1));
 
-    this.questionsFetch$ =this._questionsService.getQuestionsOfSubSection(this.subSectionId).pipe(tap(vals => {
-      this.questions =vals;
-    }))
+    this.sectionInfo$ = this._questionsService.getSingleSubsection(this.subSectionId);
+    this.questionsFetch$ = this._questionsService.getQuestionsOfSubSection(this.subSectionId)
+
+    return combineLatest([this.sectionInfo$, this.questionsFetch$])
+
+  }), tap(([subsectionInfo, questions]) => {
+    this.subsectionName = subsectionInfo.name;
+    this.questions = questions
   }))
 
 
