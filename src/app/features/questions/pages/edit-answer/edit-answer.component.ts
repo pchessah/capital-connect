e4628a4dc99ae6b9a748cfda1f3ffc2c5eaa0e50
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-27 14:30:14 +0300

    added question edit functionality

--- src/app/features/questions/pages/edit-answer/edit-answer.component.ts
+++ src/app/features/questions/pages/edit-answer/edit-answer.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Answer, AnswerInput, Question} from "../../interfaces";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormStateService} from "../../services/form-state/form-state.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {QuestionsService} from "../../services/questions/questions.service";
+import {Observable, tap} from "rxjs";
+import {UiComponent} from "../../components/ui/ui.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-edit-answer',
   standalone: true,
-  imports: [],
+  imports: [
+    UiComponent,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './edit-answer.component.html',
   styleUrl: './edit-answer.component.scss'
 })
 export class EditAnswerComponent {
+  answerFormIsValid = false;
+  question!: Question;
 
+  private _fb = inject(FormBuilder)
+  private _formStateService = inject(FormStateService)
+  private _router = inject(Router)
+  private _activatedRoute = inject(ActivatedRoute);
+  private _questionsService = inject(QuestionsService);
+  private _routeId!:string;
+  question$ =new Observable<Question>()
+  questionId!: number;
+  answerForm = this._fb.group({
+    text: ['', Validators.required],
+    weight: ['', [Validators.required, Validators.min(0)]],
+    questionId: [null as any, Validators.required]
+  });
+
+  questions$ = this._activatedRoute.params.pipe(tap((params) => {
+    const ids = params['id'].split('-')
+    this.questionId = Number(ids.at(-1));
+    this._routeId =ids.slice(0, 2).join('-')
+    this.answerForm.patchValue({ questionId: this.questionId});
+    this.question$ = this._questionsService.getSingleQuestion(this.questionId).pipe(tap(quiz => {
+      this.question =quiz;
+    }))
+  }))
+
+  answerForm$ = this.answerForm.valueChanges.pipe(tap(vals => {
+    const answerInput = {
+      text: vals.text,
+      weight: Number(vals.weight),
+      questionId: Number(this.questionId)
+    }
+    this._formStateService.setAnswerForm(answerInput as AnswerInput)
+    this._formStateService.setAnswerFormIsValid(this.answerForm.valid)
+  }))
+
+  answerFormIsValid$ = this._formStateService.answerFormIsValid$.pipe(tap(isValid => this.answerFormIsValid = isValid))
+
+  submit$: Observable<Answer> = new Observable();
+
+  submit() {
+    this.submit$ = this._formStateService.createAnswer(this.questionId).pipe(tap(res => {
+      this.answerForm.reset();
+    }));
+  }
+
+  cancel(){
+    this._router.navigateByUrl(`/questions/sub-section/${this._routeId}`);
+  }
 }
