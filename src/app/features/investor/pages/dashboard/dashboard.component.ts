d9ea82ef34dc0201fd72c7787cb22dd57fe818f9
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-28 19:15:54 +0300

    created the dashboard

--- src/app/features/investor/pages/dashboard/dashboard.component.ts
+++ src/app/features/investor/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import {MainComponent} from "../../../business/components/dashboard/main/main.component";
 import {SidenavComponent} from "../../../../core";
+import {MainComponent} from "../../components/dashboard/main/main.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    MainComponent,
-    SidenavComponent
+    SidenavComponent,
+    MainComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
