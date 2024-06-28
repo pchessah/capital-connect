6ba93defbe51ced2cc454ea099b9c06c2f2b6521
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-28 11:33:47 +0300

    created  a screen service

---
+++ src/app/features/investor/services/investor.screens.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from "@angular/core";
+import {BehaviorSubject} from "rxjs";
+import {FormSteps, InvestorScreen} from "../interfaces/investor.sections.enum";
+
+@Injectable(
+  {
+    providedIn: 'root'
+  }
+)
+
+export class InvestorScreensService {
+  private _currentScreenSRC =new BehaviorSubject<number>(1)
+  private _currentStepSRC =new BehaviorSubject<number>(1);
+
+  currentScreen$ =this._currentScreenSRC.asObservable();
+  currentStep$ =this._currentStepSRC.asObservable();
+
+  setCurrentScreen(screen: number) {
+    this._currentScreenSRC.next(screen);
+  }
+
+  setCurrentStep(step: number){
+    this._currentStepSRC.next(step);
+  }
+}
