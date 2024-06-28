8ac09226d71976931bb02d97f9b94e84c389be44
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-28 09:45:16 +0300

    used dynamic investor onboarding questions

--- src/app/features/investor/pages/onboarding/onboarding.component.ts
+++ src/app/features/investor/pages/onboarding/onboarding.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {LayoutComponent} from "../../../../shared/business/layout/layout.component";
+import {IndexLayoutComponent} from "../../../../shared/business/components/index-layout/index-layout.component";
+import {LandingComponent} from "../../components/onboarding/landing/landing.component";
 
 @Component({
   selector: 'app-onboarding',
   standalone: true,
-  imports: [],
+  imports: [
+    LayoutComponent,
+    IndexLayoutComponent,
+    LandingComponent
+  ],
   templateUrl: './onboarding.component.html',
   styleUrl: './onboarding.component.scss'
 })
