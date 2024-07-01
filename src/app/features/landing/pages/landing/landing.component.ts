7bb8b2a4f88c1e0eafe25239741a80bb832b5119
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-02 01:29:26 +0300

    added the forgot password screen to the landing page

--- src/app/features/landing/pages/landing/landing.component.ts
+++ src/app/features/landing/pages/landing/landing.component.ts
@@ -5,11 +5,14 @@ import { CarouselComponent } from '../../../carousel/carousel.component';
 import { LogInFormComponent } from '../../../auth/components/log-in-form/log-in-form.component';
 import { SignUpFormComponent } from '../../../auth/components/sign-up-form/sign-up-form.component';
 import { FORM_TYPE } from '../../../auth/interfaces/auth.interface';
+import {
+  ForgotPasswordFormComponent
+} from "../../../auth/components/forgot-password-form/forgot-password-form.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, SignUpFormComponent, LogInFormComponent, CarouselComponent],
+  imports: [CommonModule, SignUpFormComponent, LogInFormComponent, CarouselComponent, ForgotPasswordFormComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
@@ -18,7 +21,7 @@ export class LandingComponent {
   private _router = inject(Router);
   activeRouteData: { mode: FORM_TYPE } = this._router.getCurrentNavigation()?.extras.state as any
   forms = FORM_TYPE;
-  activeForm = this.activeRouteData?.mode === FORM_TYPE.SIGNIN ? FORM_TYPE.SIGNIN : FORM_TYPE.SIGNUP;
+  activeForm = this.activeRouteData?.mode === FORM_TYPE.SIGNIN ? FORM_TYPE.SIGNIN : FORM_TYPE.SIGNUP? FORM_TYPE.SIGNUP: FORM_TYPE.FORGOT_PASSWORD;
 
   setActiveForm(formType: FORM_TYPE){
     this.activeForm = formType;
