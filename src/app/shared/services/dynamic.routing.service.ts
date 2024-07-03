ebf190999daafd864982249349cd9a220fdadb99
Meltus Bwire <165395433+j-netcom-dev@users.noreply.github.com>
2024-07-03 09:12:10 +0300

    Fix investor jumping (#77)
    * WIP
    
    * Auth pages styling (#75)
    
    * #chore:
    - styled forgot password button to look muted
    - emited a back event after forgot password request has been made
    
    * #feat
    - made the welcome screen reusable
    
    * #refactor
    
    the landing section uses the welcome template
    
    * made the styles of the auth forms reusable
    
    * styled the password reset pages
    
    * #chore:
    - styled the verify email screen
    
    * #feat
    
    - implemented to check the next step
    
    * tested investor eligibility
    
    * modified submissions
    
    * removed steps changes
    
    * added a profile page to route user to the correct page in the event no forms were filled
    
    * #feat:
    routed the profile page
    
    * redirect logged in Users to the profile page
    
    * #chore:
    added default states to the root pages
    
    * #fix:
    fixed investor page hoping
    
    ---------
    
    Co-authored-by: Paul Chesa <paulchesa1@gmail.com>

--- src/app/shared/services/dynamic.routing.service.ts
+++ src/app/shared/services/dynamic.routing.service.ts
@@ -1,14 +1,19 @@
 import {inject, Injectable} from "@angular/core";
 import {SubMissionStateService} from "../business/services/submission-state.service";
-import {map} from "rxjs/operators";
+import {map, switchMap} from "rxjs/operators";
 import {
-  BUSINESS_FINANCIALS_SUBSECTION_IDS, ESUBSECTIONS, getInvestorEligibilitySubsectionIds,
+  BUSINESS_FINANCIALS_SUBSECTION_IDS,
+  ESUBSECTIONS,
+  getInvestorEligibilitySubsectionIds,
+  INVESTOR_ONBOARDING_SUBSECTION_IDS,
   INVESTOR_PREPAREDNESS_SUBSECTION_IDS,
   ISCORE,
   ISECTION
 } from "../business/services/onboarding.questions.service";
 import {GrowthStage} from "../../features/organization/interfaces";
-
+import { UserSubmissionResponse} from "../interfaces/submission.interface";
+import {QuestionsService} from "../../features/questions/services/questions/questions.service";
+import {SubSection} from "../../features/questions/interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +21,39 @@ import {GrowthStage} from "../../features/organization/interfaces";
 
 export class DynamicRoutingService {
   private _submissionStateService =inject(SubMissionStateService);
+  private _questionService =inject(QuestionsService)
 
   getInvestorSubmissions(){
-    return this._submissionStateService.getUserSubmissions().pipe(map(submissions => {
-      return submissions.length > 0;
+    return this._questionService.getSectionQuestions(INVESTOR_ONBOARDING_SUBSECTION_IDS.ID).pipe(switchMap(questions =>{
+      return this._submissionStateService.getUserSubmissions().pipe(map(submissions => {
+        for(let id of Object.keys(INVESTOR_ONBOARDING_SUBSECTION_IDS)){
+          let subsectionID =0
+          switch(id as ESUBSECTIONS) {
+            case ESUBSECTIONS.LANDING:
+              subsectionID =INVESTOR_ONBOARDING_SUBSECTION_IDS.LANDING
+              if(!this.subsectionSubmitted(subsectionID, submissions, questions.find(question =>question.id ==subsectionID).questions || []))
+                return ['/investor/onboarding', [1]]
+              break
+            case ESUBSECTIONS.STEP_ONE:
+              subsectionID =INVESTOR_ONBOARDING_SUBSECTION_IDS.STEP_ONE
+              if(!this.subsectionSubmitted(subsectionID, submissions, questions.find(question =>question.id ==subsectionID).questions || []))
+                return ['/investor/onboarding', [2, 1]]
+              break
+            case ESUBSECTIONS.STEP_TWO:
+              subsectionID =INVESTOR_ONBOARDING_SUBSECTION_IDS.STEP_TWO
+              if(!this.subsectionSubmitted(subsectionID, submissions, questions.find(question =>question.id ==subsectionID).questions || []))
+                return ['/investor/onboarding', [2, 2]]
+                break
+            case ESUBSECTIONS.STEP_THREE:
+              subsectionID =INVESTOR_ONBOARDING_SUBSECTION_IDS.STEP_THREE
+              if(!this.subsectionSubmitted(subsectionID, submissions, questions.find(question =>question.id ==subsectionID).questions || []))
+                return ['/investor/onboarding', [2, 3]]
+              break
+          }
+
+         }
+        return ['/investor']
+      }))
     }))
   }
   getUserSubmissions(companyGrowthStage: GrowthStage){
@@ -60,7 +94,12 @@ export class DynamicRoutingService {
     }
     return []
   }
-
+  subsectionSubmitted(id:number, submissions: any[], questions:any[]){
+    // @ts-ignore
+    const investorSubmissions =submissions.filter(submission =>{ return submission.question.subSection.id ==id })
+    console.log(investorSubmissions, questions, id)
+    return (investorSubmissions.length >0 && questions.length >0) || (investorSubmissions.length ==0 && questions.length ==0)
+  }
   isAnswered(subsection?:ISCORE){
     if(!subsection) return true
     return (subsection.score == 0 && subsection.targetScore == 0) || (subsection.score > 0);
