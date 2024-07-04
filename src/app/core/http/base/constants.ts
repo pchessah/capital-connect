52da8a0cd25a35ec5e3b5a9ddcc63f8a1735690a
Paul Chesa <paulchesa1@gmail.com>
2024-07-04 10:16:08 +0300

    test api connect api

--- src/app/core/http/base/constants.ts
+++ src/app/core/http/base/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://capitalconnect-0060e0fb0eb4.herokuapp.com" //This is staging
-// export const BASE_URL = "http://api.capitalconnect.africa"
+// export const BASE_URL = "https://capitalconnect-0060e0fb0eb4.herokuapp.com" //This is staging
+export const BASE_URL = "http://api.capitalconnect.africa"
