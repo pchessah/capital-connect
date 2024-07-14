c9ef5d0cd159db50e4be0d505f8c970c67dc06f8
Paul Chesa <paulchesa1@gmail.com>
2024-07-14 21:42:30 +0300

    Feedback notifier (#97)
    * feat: Create component to show message
    
    * feat: Update service for feedback

--- src/app/core/services/feedback/feedback.service.ts
+++ src/app/core/services/feedback/feedback.service.ts
@@ -1,47 +1,42 @@
-import { inject, Injectable, OnDestroy } from '@angular/core';
-import { FeedbackComponent } from '../../components/feedback/feedback.component';
-import { MatDialog } from '@angular/material/dialog';
-import { Subject, takeUntil } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService implements OnDestroy {
 
-  private dialog = inject(MatDialog);
+  private messageSubject = new BehaviorSubject<{ title: string, message: string, type: 'info' | 'success' | 'warning' | 'error' } | null>(null);
+  message$ = this.messageSubject.asObservable();
 
   private destroy$ = new Subject<boolean>();
 
-  openDialog(title: string, message: string, type: 'info' | 'success' | 'warning' | 'error') {
-    const ref = this.dialog.open(FeedbackComponent, {
-      width: '35vw',
-      data: { title, message, type },
-    });
+  private openMessage(title: string, message: string, type: 'info' | 'success' | 'warning' | 'error') {
+    this.messageSubject.next({ title, message, type });
 
-    ref.afterOpened().pipe(takeUntil(this.destroy$)).subscribe(() => {
-      setTimeout(() => {
-        ref.close();
-      }, 3000)
-    })
+    setTimeout(() => {
+      this.messageSubject.next(null);
+    }, 3000);
   }
 
   info(message: string, title = 'Information') {
-    this.openDialog(title, message, 'info');
+    this.openMessage(title, message, 'info');
   }
 
   success(message: string, title = 'Success') {
-    this.openDialog(title, message, 'success');
+    this.openMessage(title, message, 'success');
   }
 
   warning(message: string, title = 'Warning') {
-    this.openDialog(title, message, 'warning');
+    this.openMessage(title, message, 'warning');
   }
 
   error(message: string, title = 'Error') {
-    this.openDialog(title, message, 'error');
+    this.openMessage(title, message, 'error');
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
