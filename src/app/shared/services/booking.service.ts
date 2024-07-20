958ec082fdf50309961c43f7e7ae3901d264f7e4
Trevor-crypto243 <otitrevor450@gmail.com>
2024-07-20 13:50:39 +0300

    Picking the Bearer token from the authstateService

--- src/app/shared/services/booking.service.ts
+++ src/app/shared/services/booking.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable ,inject} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BaseHttpService } from '../../core/http/base/base.http.service';
 import { CreateBookingRequest, CreateBookingResponse } from '../interfaces/booking';
 import { BASE_URL } from '../../core';
 import { Router } from '@angular/router';
+import { AuthStateService } from '../../features/auth/services/auth-state.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService extends BaseHttpService {
+  authStateService = inject(AuthStateService);
+  token = this.authStateService.authToken;
   constructor(private _httpClient: HttpClient, private router: Router) {
     super(_httpClient);
   }
 
   private headers = new HttpHeaders({
     'Content-Type': 'application/json; charset=utf-8',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpyZGFuZGlhYmVsdGFAZ21haWwuY29tIiwic3ViIjo2LCJyb2xlcyI6WyJ1c2VyIl0sImlhdCI6MTcyMTIxMTgwMiwiZXhwIjoxNzIxMjk4MjAyfQ.6Vr0fX7wDsTBnRdeOp35Me7ki6_rA6Tf-jBue75KHm0'
+    'Authorization': `Bearer ${this.token}`
   });
 
 
