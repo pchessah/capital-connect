8de43f1ca7bf542d5c0f591440f079c23bce30c9
Paul Chesa <paulchesa1@gmail.com>
2024-07-24 20:30:12 +0300

    feat: Added navbar check for admin users

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { booleanAttribute, Component, inject, Input } from '@angular/core';
-import { SharedModule } from "../../../shared";
+import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SharedModule } from "../../../shared";
 import { AuthStateService } from '../../../features/auth/services/auth-state.service';
 import { CompanyStateService } from '../../../features/organization/services/company-state.service';
-import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-navbar',
@@ -24,14 +23,14 @@ export class NavbarComponent {
   logOut$ = new Observable<boolean>();
 
   @Input({ transform: booleanAttribute }) on_dashboard: boolean = false;
-  
+  @Input() isAdmin = false;
+
   drawer_showing = false;
-  toggleDrawer() { 
-    this.drawer_showing = !this.drawer_showing; 
+  toggleDrawer() {
+    this.drawer_showing = !this.drawer_showing;
   }
 
   logOut() {
     this.logOut$ = this._authService.logout()
-
   }
 }
