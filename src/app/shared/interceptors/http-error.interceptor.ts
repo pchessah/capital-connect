cf96861a8666cc7184dd4b68ded3437cce0c944f
Paul Chesa <paulchesa1@gmail.com>
2024-07-24 12:09:25 +0300

    fix: Catch error for verifying email for new logins

--- src/app/shared/interceptors/http-error.interceptor.ts
+++ src/app/shared/interceptors/http-error.interceptor.ts
@@ -1,14 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { EMPTY, throwError } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
 import { FeedbackService } from '../../core';
-import {Router} from "@angular/router";
 
 export const HttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const feedbackService = inject(FeedbackService);
-  const router =inject(Router)
+  const router = inject(Router)
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
@@ -16,14 +16,20 @@ export const HttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
         feedbackService.warning('You are unauthorized to perform the following action. Kindly Contact administrator.');
         return EMPTY;
       }
-      if(isValidCompanyOwnerPath(error.url as string)){
-        if(error.error.statusCode === 404){
+      if (isValidCompanyOwnerPath(error.url as string)) {
+        if (error.error.statusCode === 404) {
           router.navigateByUrl('/organization/setup')
         }
         feedbackService.info('Kindly add your company details.')
         return EMPTY;
       }
 
+      if (isValidAuthLoginPath(error.url as string)) {
+        if (error.error.message.includes('Your email is not verified')) {
+          router.navigateByUrl('/verify-email')
+        }
+      }
+      
       let errorMessage = 'An unknown error occurred!';
 
       if (error?.error?.message) {
@@ -40,6 +46,10 @@ export const HttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
 function isValidCompanyOwnerPath(path: string): boolean {
   const regex = /^.+\/company\/owner\/\d+$/;
   return regex.test(path);
+}
+
+function isValidAuthLoginPath(path: string): boolean {
+  return path.includes('login');
 }
 
 
