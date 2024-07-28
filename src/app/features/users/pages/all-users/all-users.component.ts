f51443e76b6e9edf35819b77982fe8495852bbb7
Paul Chesa <paulchesa1@gmail.com>
2024-07-28 17:06:05 +0300

    feat: Added view for all users using primeng

--- src/app/features/users/pages/all-users/all-users.component.ts
+++ src/app/features/users/pages/all-users/all-users.component.ts
@@ -1,60 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
 import { Observable, tap } from 'rxjs';
+import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonModule } from 'primeng/button';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { SharedModule } from '../../../../shared';
 import { AdminUiContainerComponent } from '../../../admin/components/admin-ui-container/admin-ui-container.component';
 import { User } from '../../models';
 import { UsersHttpService } from '../../services/users-http.service';
+import { FormsModule } from '@angular/forms';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-all-users',
   standalone: true,
-  imports: [CommonModule, AdminUiContainerComponent, TableModule, SharedModule,
-    ButtonModule,
-    InputTextModule,
-    TooltipModule],
+  imports: [CommonModule, AdminUiContainerComponent, TableModule, FormsModule, ButtonModule, InputTextModule, TooltipModule],
   templateUrl: './all-users.component.html',
-  styleUrl: './all-users.component.scss'
+  styleUrls: ['./all-users.component.scss']
 })
 export class AllUsersComponent implements AfterViewInit {
 
-  users: User[] = [];
-  cols: any[] = [];
-
   private _usersService = inject(UsersHttpService);
 
+  users: User[] = [];
   users$ = new Observable<User[]>();
-  displayedColumns: string[] = ['firstName', 'lastName', 'roles', 'isEmailVerified', 'actions'];
-  dataSource!: MatTableDataSource<User>;
+  cols: any[] = [
+    { field: 'firstName', header: 'First Name' },
+    { field: 'lastName', header: 'Last Name' },
+    { field: 'roles', header: 'Roles' },
+    { field: 'isEmailVerified', header: 'Email Verified' },
+    { field: 'actions', header: 'Actions' }
+  ];
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild('dt') table!: Table;
 
   ngAfterViewInit(): void {
     this.users$ = this._usersService.getAllUsers().pipe(
       tap(users => {
         this.users = users;
-        this.dataSource = new MatTableDataSource(users);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
       })
     );
+    this.users$.subscribe();
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+    this.table.filterGlobal(filterValue, 'contains');
   }
 
   editUser(user: User) {
