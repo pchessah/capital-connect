a36fd6c89d7af19b584b94e16951604475cbe3e3
Trevor-crypto243 <otitrevor450@gmail.com>
2024-07-28 21:11:34 +0300

    UI updates

--- src/app/features/investor/components/InvestorProfile/investor-profile.component.ts
+++ src/app/features/investor/components/InvestorProfile/investor-profile.component.ts
@@ -13,7 +13,7 @@ import { ModalComponent } from '../../../../shared/components/modal/modal.compon
 @Component({
   selector: 'app-investor-profile',
   standalone: true,
-  imports: [ CommonModule,FormsModule, NavbarComponent ,ModalComponent],
+  imports: [CommonModule, FormsModule, NavbarComponent, ModalComponent],
   templateUrl: './investor-profile.component.html',
   styleUrls: ['./investor-profile.component.scss']
 })
@@ -26,37 +26,38 @@ export class InvestorProfileComponent {
   selectedProfile: InvestorProfile | null = null;
   newProfile: InvestorProfile = {} as InvestorProfile;
   visible = false;
-  editMode:boolean = false
+  editMode: boolean = false
 
 
 
-  
+
   investorProfiles$ = this._investorProfileService.getAllInvestorProfiles().pipe(tap(profiles => this.investorProfiles = profiles))
-  // investorProfile$ = this._investorProfileService.getInvestorProfileById(1).pipe(tap(profiles => this.investorProfiles = profiles))
 
-    
-  viewProfile(userId: number){
-    this.visible =true
-    this.investorProfile= {
-        userId: 1,
-        organizationName: "Test Name",
-        countriesOfInvestmentFocus: ["Kenya","Namibia","Sudan"],
-        headOfficeLocation: "NewYork",
-        emailAddress: "otitrevor450@gmail.com",
-        contactPerson: "Trevor Ogina",
-        useOfFunds: ["Accounting", "Finance","Sales","Tech","Customer Service"],
-        maximumFunding: 120909090090,
-        minimumFunding: 93092092,
-        sectorsOfInvestment: ["Agriculture", "Engineering","Health"],
-        businessGrowthStages: ["Beginer", "Amateur", "Experienced"],
-        investorType: "Personal",
-        investmentStructures: ["Structure 1","Structure 2"],
-        esgFocusAreas: ["Test","Focus","Area"],
-        registrationStructures:["Test","Focus","Area"],
-      
+
+  viewProfile(userId: number) {
+    // investorProfile$ = this._investorProfileService.getInvestorProfileById(1).pipe(tap(profiles => this.investorProfile = profiles))
+
+    this.visible = true
+    this.investorProfile = {
+      userId: 1,
+      organizationName: "Test Name",
+      countriesOfInvestmentFocus: ["Kenya", "Namibia", "Sudan"],
+      headOfficeLocation: "NewYork",
+      emailAddress: "otitrevor450@gmail.com",
+      contactPerson: "Trevor Ogina",
+      useOfFunds: ["Accounting", "Finance", "Sales", "Tech", "Customer Service"],
+      maximumFunding: 120909090090,
+      minimumFunding: 93092092,
+      sectorsOfInvestment: ["Agriculture", "Engineering", "Health"],
+      businessGrowthStages: ["Beginer", "Amateur", "Experienced"],
+      investorType: "Personal",
+      investmentStructures: ["Structure 1", "Structure 2"],
+      esgFocusAreas: ["Test", "Focus", "Area"],
+      registrationStructures: ["Test", "Focus", "Area"],
+
     }
   }
-  
+
 
   showDialog() {
     this.visible = true;
@@ -68,20 +69,29 @@ export class InvestorProfileComponent {
     this.visible = true;
   }
 
-  saveProfile() {
-    if (this.selectedProfile) {
-      this._investorProfileService.updateInvestorProfileById(this.selectedProfile.userId, this.selectedProfile)
+  // saveProfile() {
+  //   createInvestorProfile$ = this._investorProfileService.createInvestorProfile({}).pipe(tap());
 
-    } else {
-      this._investorProfileService.createInvestorProfile(this.newProfile)
+  //   if (this.selectedProfile) {
+  //     this._investorProfileService.updateInvestorProfileById(this.selectedProfile.userId, this.selectedProfile)
 
-    }
-  }
+  //   } else {
+  //     this._investorProfileService.createInvestorProfile(this.newProfile)
 
-  deleteProfile(id: number) {
-    this._investorProfileService.deleteInvestorProfileById(id)
-      // .subscribe(() => this.loadInvestorProfiles());
-  }
+  //   }
+  // }
+
+  // updateProfile() {
+  //   updateInvestorProfile$ = this._investorProfileService.updateInvestorProfileById({}).pipe();
+
+  // }
+
+  // deleteProfile(id: number) {
+  //   deleteInvestorProfile$ = this._investorProfileService.deleteInvestorProfileById(1).pipe();
+
+  //   this._investorProfileService.deleteInvestorProfileById(id)
+  //   // .subscribe(() => this.loadInvestorProfiles());
+  // }
 
   cancel() {
     this.visible = false;
