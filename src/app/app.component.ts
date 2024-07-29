7ad303005088d49f5391b972abf57203fa9437ce
Paul Chesa <paulchesa1@gmail.com>
2024-07-29 22:06:37 +0300

    chore: Reduce boiler plate for checking loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { distinctUntilChanged, tap } from 'rxjs';
 import { SharedModule } from './shared';
-import { LoadingComponent } from './core/components/loading/loading.component';
-import { FeedbackNotificationComponent, LoadingService } from './core';
+import { } from './core/components/loading/loading.component';
+import { FeedbackNotificationComponent, LoadingService, LoadingComponent } from './core';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +13,16 @@ import { FeedbackNotificationComponent, LoadingService } from './core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
 
   private _loadingService = inject(LoadingService);
   private _cd = inject(ChangeDetectorRef);
-  
-  isLoading = true;
 
-  ngOnInit(): void {
-    this._trackLoadingStatusSubscription();
-  }
-
-  private _trackLoadingStatusSubscription = () => this._loadingService.loading$.subscribe(res => {
+  loading$ = this._loadingService.loading$.pipe(distinctUntilChanged(), tap(res => {
     this.isLoading = res;
     this._cd.detectChanges();
-  })
+  }))
 
-  ngOnDestroy(): void {
-    this._trackLoadingStatusSubscription().unsubscribe()
-  }
-
-
-
+  isLoading = true;
 
 }
