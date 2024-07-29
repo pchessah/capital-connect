019847c4eafb364ed606c73510e5adce36d837b5
Paul Chesa <paulchesa1@gmail.com>
2024-07-29 22:07:13 +0300

    fix: Change initial loading to true

--- src/app/core/services/loading/loading.service.ts
+++ src/app/core/services/loading/loading.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class LoadingService {
-  private _loading = new BehaviorSubject<boolean>(false);
+  private _loading = new BehaviorSubject<boolean>(true);
   public readonly loading$ = this._loading.asObservable();
 
   setLoading(loading: boolean): void {
