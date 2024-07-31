0cd21b023768a8e519294c827e2f19573b0a34f7
Paul Chesa <paulchesa1@gmail.com>
2024-07-31 20:31:53 +0300

    feat: Finish user submissions section on admin

--- src/app/features/organization/components/organization-submissions-info/organization-submissions-info.component.ts
+++ src/app/features/organization/components/organization-submissions-info/organization-submissions-info.component.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { TabViewModule } from 'primeng/tabview';
 import { User } from '../../../users/models';
 import { OrganizationInfoContainerComponent } from "../organization-info-container/organization-info-container.component";
-import { Observable, tap } from 'rxjs';
 import { SubmissionService, UserSubmissionResponse } from '../../../../shared';
 import { BUSINESS_INFORMATION_SUBSECTION_IDS, getInvestorEligibilitySubsectionIds, INVESTOR_PREPAREDNESS_SUBSECTION_IDS } from '../../../../shared/business/services/onboarding.questions.service';
-import { Company, CompanyResponse } from '../../interfaces';
+import { CompanyResponse } from '../../interfaces';
 
 @Component({
   selector: 'app-organization-submissions-info',
   standalone: true,
-  imports: [CommonModule, OrganizationInfoContainerComponent],
+  imports: [CommonModule, OrganizationInfoContainerComponent, TabViewModule],
   templateUrl: './organization-submissions-info.component.html',
   styleUrl: './organization-submissions-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,6 +19,15 @@ import { Company, CompanyResponse } from '../../interfaces';
 })
 export class OrganizationSubmissionsInfoComponent implements OnChanges {
   owner!: User;
+  investorPreparednessResponses: UserSubmissionResponse[] = [];
+  businessResponses: UserSubmissionResponse[]= [];
+  investorEligibilityResponses: UserSubmissionResponse[]= [];
+
+  activeTab: string = 'investorEligibility';
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['company'] && changes['company'].currentValue) {
@@ -25,28 +35,27 @@ export class OrganizationSubmissionsInfoComponent implements OnChanges {
       this.owner = this.company.user
 
       this.investorEligilityResponses$ = this._userSubmissionsService.fetchSubmissionsByUserPerSection(this.owner.id, getInvestorEligibilitySubsectionIds(this.company.growthStage).ID).pipe(tap(res => {
-        debugger
-      })) 
-      
+        this.investorEligibilityResponses = res
+      }))
+
       this.businessResponses$ = this._userSubmissionsService.fetchSubmissionsByUserPerSection(this.owner.id, BUSINESS_INFORMATION_SUBSECTION_IDS.ID).pipe(tap(res => {
-        debugger
-      })) 
-      
-      this. investorPreparednessResponses$ = this._userSubmissionsService.fetchSubmissionsByUserPerSection(this.owner.id, INVESTOR_PREPAREDNESS_SUBSECTION_IDS.ID).pipe(tap(res => {
-        debugger
-      })) 
+        this.businessResponses = res
+      }))
+
+      this.investorPreparednessResponses$ = this._userSubmissionsService.fetchSubmissionsByUserPerSection(this.owner.id, INVESTOR_PREPAREDNESS_SUBSECTION_IDS.ID).pipe(tap(res => {
+        this.investorPreparednessResponses = res
+      }))
     }
   }
 
-  @Input({ required: true}) company!: CompanyResponse
-
+  @Input({ required: true }) company!: CompanyResponse
 
   private _userSubmissionsService = inject(SubmissionService);
 
   submissions$: Observable<UserSubmissionResponse[]> = new Observable();
 
   investorEligilityResponses$: Observable<UserSubmissionResponse[]> = new Observable();
-  businessResponses$:Observable<UserSubmissionResponse[]> = new Observable();
-  investorPreparednessResponses$:Observable<UserSubmissionResponse[]> = new Observable();
+  businessResponses$: Observable<UserSubmissionResponse[]> = new Observable();
+  investorPreparednessResponses$: Observable<UserSubmissionResponse[]> = new Observable();
 
 }
